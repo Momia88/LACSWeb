fd497bc11de1baafb81756cbc9408d8e56802b1e
Morris Chen <morris.chen@coretronic.com>
2016-12-26 09:59:49 +0800

    Change Title

--- js/Variables.js
+++ js/Variables.js
@@ -2,6 +2,6 @@
  *
  */
 
-// var serverIP = "http://10.16.0.68:8080/LACSProj/api/v1";
+//var serverIP = "http://10.16.0.68:8080/LACSWebService-1.0/api/v1";
 var serverIP = "http://localhost:8080/LACSWebService-1.0/api/v1";
 
